ccba05fb27ab8a29613c95ee884a23100a6c903c
christopherwj <38334130+christopherwj@users.noreply.github.com>
2026-09-16 17:14:05 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ranges>
+#include <string>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::ranges::sort(s);
+        std::ranges::sort(t);
+    return s == t;
+    }
+};
